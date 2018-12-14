a81dc39e3bd4bb7b6d8f90b85243e2aaad19ebed
I Mertz <imertz@protonmail.com>
2018-12-14 20:34:03 +0200

    implemented functionality up to six digits

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const data = require('./data')
 
 module.exports = function (num) {
   num = num.toString()
+  if (num.length > 6 && num.length < 10) {
+    let fourDigitString=''
+    if (num.length === 4 && num[0] === '1') {
+      fourDigitString = 'χίλια'
+    } else {
+      const fourDigitNumber = num.slice(0, num.length - 3)
+      fourDigitString = `${data.sixDigits[parseInt(fourDigitNumber.split('').shift())]} ${data.fourOrFiveDigits[parseInt(fourDigitNumber.slice(1, 3))]} χιλιάδες`
+    }
+  if (num.length > 3 && num.length < 7) {
+    let fourDigitString
+    if (num.length === 4 && num[0] === '1') {
+      fourDigitString = 'χίλια'
+    } else {
+      const fourDigitNumber = num.slice(0, num.length - 3)
+      fourDigitString = `${data.sixDigits[parseInt(fourDigitNumber.split('').shift())]} ${data.fourOrFiveDigits[parseInt(fourDigitNumber.slice(1, 3))]} χιλιάδες`
+    }
+
+    const threeDigitNumber = num.slice(-3)
+    return `${fourDigitString} ${data.tripleDigit[parseInt(threeDigitNumber.split('').shift())]} ${data.singleOrDoubleDigit[parseInt(threeDigitNumber.slice(1, 3))]}`
+  }
   if (num.length === 3) {
     return `${data.tripleDigit[parseInt(num.split('').shift())]} ${data.singleOrDoubleDigit[parseInt(num.slice(1, 3))]}`
   } else {
